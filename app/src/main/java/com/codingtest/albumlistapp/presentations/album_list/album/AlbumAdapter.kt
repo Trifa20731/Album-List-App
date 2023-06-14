520bb9229cfd18a91e73cdab9519c4c64a2d8a8d
trifa <jasonj20731@gmail.com>
2023-06-14 16:33:25 +0800

    ADD: Add album cover display and finish album item contents.

--- app/src/main/java/com/codingtest/albumlistapp/presentations/album_list/album/AlbumAdapter.kt
+++ app/src/main/java/com/codingtest/albumlistapp/presentations/album_list/album/AlbumAdapter.kt
@@ -1,9 +1,9 @@
 package com.codingtest.albumlistapp.presentations.album_list.album
 
-import android.util.Log
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import com.codingtest.albumlistapp.entities.Album
+import timber.log.Timber
 
 class AlbumAdapter: ListAdapter<Album, AlbumViewHolder>(AlbumDiffCallback()) {
 
@@ -11,17 +11,14 @@ class AlbumAdapter: ListAdapter<Album, AlbumViewHolder>(AlbumDiffCallback()) {
         private const val LOG_TAG: String = "AlbumAdapter"
     }
 
-    /**
-     * Aim to render UI according to the given style.
-     * */
     override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
-        Log.d(LOG_TAG, "OnBindViewHolder: Run.")
+        Timber.tag(LOG_TAG).d("OnBindViewHolder: Run.")
         val item = getItem(position)
         holder.bind(item)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder {
-        Log.d(LOG_TAG, "onCreateViewHolder: Run.")
+        Timber.tag(LOG_TAG).d("onCreateViewHolder: Run.")
         return AlbumViewHolder.from(parent)
     }
 
